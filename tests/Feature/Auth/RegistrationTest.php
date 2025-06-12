8e28a66abb565e671e88bb0f579c114c9e2906aa
legecha <code@legecha.com>
2025-06-12 21:00:00 +0100

    Set the password requiremenets

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -17,8 +17,8 @@ test('new users can register', function () {
     $response = Livewire::test(Register::class)
         ->set('name', 'Test User')
         ->set('email', 'test@example.com')
-        ->set('password', 'password')
-        ->set('password_confirmation', 'password')
+        ->set('password', 'pass123WORD!@£')
+        ->set('password_confirmation', 'pass123WORD!@£')
         ->call('register');
 
     $response
@@ -27,3 +27,21 @@ test('new users can register', function () {
 
     $this->assertAuthenticated();
 });
+
+test('registration password must be sufficiently strong', function (string $password) {
+    $response = Livewire::test(Register::class)
+        ->set('name', 'Test User')
+        ->set('email', 'test@example.com')
+        ->set('password', $password)
+        ->set('password_confirmation', $password)
+        ->call('register');
+
+    $response->assertHasErrors(['password']);
+})->with([
+    'password',
+    'passWORD',
+    'pass123',
+    'pass123WORD',
+    'p1W!',
+    '!@£$%^&*()',
+]);
--- tests/Feature/Settings/PasswordUpdateTest.php
+++ tests/Feature/Settings/PasswordUpdateTest.php
@@ -11,34 +11,57 @@ uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 
 test('password can be updated', function () {
     $user = User::factory()->create([
-        'password' => Hash::make('password'),
+        'password' => Hash::make('pass123WORD!@£'),
     ]);
 
     $this->actingAs($user);
 
     $response = Livewire::test(Password::class)
-        ->set('current_password', 'password')
-        ->set('password', 'new-password')
-        ->set('password_confirmation', 'new-password')
+        ->set('current_password', 'pass123WORD!@£')
+        ->set('password', 'new-pass123WORD!@£')
+        ->set('password_confirmation', 'new-pass123WORD!@£')
         ->call('updatePassword');
 
     $response->assertHasNoErrors();
 
-    expect(Hash::check('new-password', $user->refresh()->password))->toBeTrue();
+    expect(Hash::check('new-pass123WORD!@£', $user->refresh()->password))->toBeTrue();
 });
 
 test('correct password must be provided to update password', function () {
     $user = User::factory()->create([
-        'password' => Hash::make('password'),
+        'password' => Hash::make('pass123WORD!@£'),
     ]);
 
     $this->actingAs($user);
 
     $response = Livewire::test(Password::class)
-        ->set('current_password', 'wrong-password')
-        ->set('password', 'new-password')
-        ->set('password_confirmation', 'new-password')
+        ->set('current_password', 'wrong-pass123WORD!@£')
+        ->set('password', 'new-pass123WORD!@£')
+        ->set('password_confirmation', 'new-pass123WORD!@£')
         ->call('updatePassword');
 
     $response->assertHasErrors(['current_password']);
 });
+
+test('changed password must be sufficiently strong', function (string $password) {
+    $user = User::factory()->create([
+        'password' => Hash::make('pass123WORD!@£'),
+    ]);
+
+    $this->actingAs($user);
+
+    $response = Livewire::test(Password::class)
+        ->set('current_password', 'pass123WORD!@£')
+        ->set('password', $password)
+        ->set('password_confirmation', $password)
+        ->call('updatePassword');
+
+    $response->assertHasErrors(['password']);
+})->with([
+    'password',
+    'passWORD',
+    'pass123',
+    'pass123WORD',
+    'p1W!',
+    '!@£$%^&*()',
+]);
